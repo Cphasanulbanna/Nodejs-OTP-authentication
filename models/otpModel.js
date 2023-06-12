e3ecabbf4052e3d419293863de370f33bd322560
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-12 15:47:16 +0530

    otp model updated

--- models/otpModel.js
+++ models/otpModel.js
@@ -1,12 +1,22 @@
 const mongoose = require("mongoose");
 
-const otpSchema = new mongoose.Schema({
-    otp: {
-        type: String,
-        min: 4,
-        max: 4,
-        required: true,
+const otpSchema = new mongoose.Schema(
+    {
+        otp: {
+            type: String,
+            min: 4,
+            max: 4,
+            required: true,
+        },
+        otpAttempts: {
+            type: Number,
+            default: 0,
+        },
+        lastOtpAttempt: {
+            type: Date,
+        },
     },
-});
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("OTP", otpSchema);
