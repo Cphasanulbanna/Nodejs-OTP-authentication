5c65cee3895d6db55213d6dc0c0ba15806cda8cb
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-06-12 15:37:03 +0530

    login completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ const OTP = require("../models/otpModel");
 //functions
 const { generateAccessToken } = require("../utils/jwt");
 const { generateOtp } = require("../utils/otp");
-const { generatePasswordHash } = require("../utils/bcrypt");
+const { generatePasswordHash, comparePassword } = require("../utils/bcrypt");
 
 const signup = async (req, res) => {
     try {
@@ -68,10 +68,6 @@ const verifyPhoneNumber = async (req, res) => {
             return res.status(404).json({ message: "User not found" });
         }
 
-        console.log(user, "user");
-        console.log(otp, "eneterd otp");
-        console.log(user.otp.otp, "db otp");
-
         if (otp !== user.otp.otp) {
             return res.status(400).json({ message: "invalid OTP" });
         }
@@ -87,13 +83,21 @@ const verifyPhoneNumber = async (req, res) => {
 
 const login = async (req, res) => {
     try {
-        const { username } = req.body;
-        if (!username) {
-            return res.status(400).json({ message: "Username is required" });
+        const { number, password } = req.body;
+        if (!number) {
+            return res.status(400).json({ message: "phone number is required" });
         }
-        const user = await User.findOne({ username: username });
+        if (!password) {
+            return res.status(400).json({ message: "password is required" });
+        }
+        const user = await User.findOne({ number: number });
         if (!user) {
             return res.status(400).json({ message: "User not found" });
+        }
+
+        const isValidPassword = await comparePassword(password, user.password);
+        if (!isValidPassword) {
+            return res.status(400).json({ message: "Invalid password" });
         }
 
         const access_token = generateAccessToken(user._id);
@@ -101,6 +105,7 @@ const login = async (req, res) => {
             message: "Login successful",
             id: user._id,
             username: user.username,
+            phone_number: user.number,
             access_token: access_token,
         });
     } catch (error) {
